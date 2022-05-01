023452afe3179cbf33037289e6e16107cb3e8c73
dipshanadh <dipshanadhikari1234@gmail.com>
2022-05-01 18:30:49 +0545

    day 1 completed

--- day1/js/app.js
+++ day1/js/app.js
@@ -1,7 +1,14 @@
-// variables
-
-// querySelectors
+// variables and  querySelectors
+const loadingBtn = document.getElementById("loadingBtn"),
+	loadingImg = document.getElementById("loadingImg")
 
 // eventListeners
+// for preloader
+window.addEventListener("load", () => {
+	setTimeout(() => {
+		loadingImg.style.display = "none"
+		loadingBtn.textContent = "Loaded !"
+	}, 500)
+})
 
 // functions
